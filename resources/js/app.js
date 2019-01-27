57bd10496f2125097e1c2ce8062d35c9a9d95905
DaanAmsterdam <daan227@gmail.com>
2019-01-27 22:00:17 +0100

    36: Data centric approach

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,14 +3,20 @@
 
 window.Vue = require('vue');
 
+Vue.prototype.authorize = function(handler) {
+    let user = window.App.user;
+
+    return user ? handler(user) : false;
+};
+
 require('./bootstrap');
 
 // all the components
 import FlashComponent from './components/Flash.vue';
-import ReplyComponent from './components/Reply.vue';
+import ThreadPage from './pages/Thread.vue';
 
 Vue.component('flash', FlashComponent);
-Vue.component('reply', ReplyComponent);
+Vue.component('thread-view', ThreadPage);
 
 // the Vue instance...
 let app = new Vue({
--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -1,77 +1,78 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">
-                        <div class="level">
-                            <span class="flex">
-                                <a href="{{ route('profile', $thread->owner ) }}">{{ $thread->owner->name }}</a>
-                                posted:
-                                {{ $thread->title }}
-                            </span>
+    <thread-view :initial-replies-count="{{ $thread->replies_count }}" inline-template>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-header">
+                            <div class="level">
+                                <span class="flex">
+                                    <a href="{{ route('profile', $thread->owner ) }}">{{ $thread->owner->name }}</a>
+                                    posted:
+                                    {{ $thread->title }}
+                                </span>
 
-                            @can('update', $thread)
-                                <form method="POST" action="{{ $thread->path() }}">
-                                    @csrf
-                                    @method('delete')
-                                    <button type="submit" class="btn btn-link">Delete Thread</button>
-                                </form>
-                            @endcan
+                                @can('update', $thread)
+                                    <form method="POST" action="{{ $thread->path() }}">
+                                        @csrf
+                                        @method('delete')
+                                        <button type="submit" class="btn btn-link">Delete Thread</button>
+                                    </form>
+                                @endcan
+                            </div>
+
                         </div>
 
+                        <div class="card-body">
+                            <div class="body">
+                                {{ $thread->body }}
+                            </div>
+                        </div>
                     </div>
 
-                    <div class="card-body">
-                        <div class="body">
-                            {{ $thread->body }}
+                    <br>
+
+                    <replies :data="{{ $thread->replies }}" @removed="repliesCount--"></replies>
+
+                    {{--{{ $replies->links() }}--}}
+
+                    @auth
+                        <form method="POST" action="{{ $thread->path() . '/replies' }}">
+                            @csrf
+                            <textarea name="body"
+                                      id="body"
+                                      class="form-control"
+                                      placeholder="Have something to say?"
+                                      rows="5">
+                            </textarea>
+
+                            <button type="submit" class="btn btn-primary">Post</button>
+                        </form>
+                    @endauth
+
+                    @guest
+                        <p class="text-center">Please <a href="{{ route('login') }}">sign in</a> to participate in this discussion.</p>
+                    @endguest
+                </div>
+
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-header">
+                           Thread Information
+                        </div>
+
+                        <div class="card-body">
+                            <div class="body">
+                                This thread was published {{ $thread->created_at->diffForHumans() }} by
+                                <a href="#">{{ $thread->owner->name }}</a> and currently
+                                has <span v-text="repliesCount"></span> {{ str_plural('reply', $thread->replies_count) }}.
+                            </div>
                         </div>
                     </div>
                 </div>
-
-                <br>
-
-                @foreach ($replies as $reply)
-                    @include('threads.reply')
-                @endforeach
-
-                {{ $replies->links() }}
-
-                @auth
-                    <form method="POST" action="{{ $thread->path() . '/replies' }}">
-                        @csrf
-                        <textarea name="body"
-                                  id="body"
-                                  class="form-control"
-                                  placeholder="Have something to say?"
-                                  rows="5">
-                        </textarea>
-
-                        <button type="submit" class="btn btn-primary">Post</button>
-                    </form>
-                @endauth
-
-                @guest
-                    <p class="text-center">Please <a href="{{ route('login') }}">sign in</a> to participate in this discussion.</p>
-                @endguest
             </div>
-
-            <div class="col-md-4">
-                <div class="card">
-                    <div class="card-header">
-                       Thread Information
-                    </div>
-
-                    <div class="card-body">
-                        <div class="body">
-                            This thread was published {{ $thread->created_at->diffForHumans() }} by
-                            <a href="#">{{ $thread->owner->name }}</a> and currently has {{ $thread->replies_count }} {{ str_plural('reply', $thread->replies_count) }}.
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-     </div>
+         </div>
+    </thread-view>
 @endsection
